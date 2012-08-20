8e63e1495634be0d1659246c66894d1baabdab86
rbs90 <rbs@hrz.tu-chemnitz.de>
2012-08-20 15:16:31 +0200

    init commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.net.MalformedURLException;
+import java.util.Timer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rbs
+ * Date: 20.08.12
+ * Time: 14:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static void main(String[] args) throws MalformedURLException {
+        SonySiteWatcher watcher = new SonySiteWatcher("https://plus.google.com/u/0/113540013167694689284/posts");
+
+        Timer timer = new Timer();
+        timer.schedule(watcher, 0, 20000);
+    }
+}
